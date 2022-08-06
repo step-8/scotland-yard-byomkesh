e57df3a92ef2253806a4f7513790c2a43920e9c2
Prafull Chavan <99485204+prafullchvn@users.noreply.github.com>
2022-08-06 10:30:27 +0530

    [#14] prafullchvn - Added script to set commit template
    What changes have you made?
    
    * Renamed setup_dir.sh to setup.sh
    * Added bash script to set default commit template.
    
    Why do we need this change?
    
    * To have consistency in commit messages.

---
+++ setup.sh
@@ -0,0 +1,2 @@
+echo "Setting Commit Template..."
+git config --local commit.template '.github/git_commit_template'
--- setup_dir.sh
+++
@@ -1,9 +0,0 @@
-mkdir src
-mkdir src/{routers,middlewares,models,handlers,utils}
-
-mkdir test
-mkdir test/{unitTest,endPointTest,APITest}
-
-touch requestLogs.txt
-
-mkdir data
