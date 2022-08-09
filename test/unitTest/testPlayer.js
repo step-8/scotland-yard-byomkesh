c08f47e79ab227dfa93296c91d32b2162199e973
sour-d <36912347+sour-d@users.noreply.github.com>
2022-08-09 11:18:24 +0530

    [#20] sourav das/sanjana - added player entity
    What changes have you made?
    
    * added player entity
    
    Why do we need this change?
    
    * to save the player info related to game

---
+++ test/unitTest/testPlayer.js
@@ -0,0 +1,50 @@
+const { Player } = require('../../src/models/player.js');
+const assert = require('assert');
+
+describe('Player entity', () => {
+  let player;
+  beforeEach(() => {
+    player = new Player('raju');
+  });
+
+  it('Should provide player info', () => {
+    const username = 'raju';
+    let role, currentPosition;
+
+    const actual = player.info;
+    const expected = { username, role, currentPosition };
+    assert.deepStrictEqual(actual, expected);
+  });
+
+  it('Should assign role to player', () => {
+    const username = 'raju';
+    let role = 'abcd', currentPosition;
+
+    player.assignRole(role);
+    const actual = player.info;
+    const expected = { username, role, currentPosition };
+    assert.deepStrictEqual(actual, expected);
+  });
+
+  it('Shouldn\'t assign role if player already have a role', () => {
+    const username = 'raju';
+    let role = 'abcd', currentPosition;
+
+    player.assignRole(role);
+    player.assignRole('xyz');
+    const actual = player.info;
+    const expected = { username, role, currentPosition };
+    assert.deepStrictEqual(actual, expected);
+  });
+
+  it('Should update player position', () => {
+    const username = 'raju';
+    let role, currentPosition = 20;
+
+    player.updatePosition(20);
+    const actual = player.info;
+    const expected = { username, role, currentPosition };
+    assert.deepStrictEqual(actual, expected);
+  });
+
+});
