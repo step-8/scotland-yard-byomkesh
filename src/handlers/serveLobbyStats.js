84f332ffc128d9fe5bf49cc531fd735f793b12aa
sour-d <36912347+sour-d@users.noreply.github.com>
2022-08-10 13:47:32 +0530

    [#20] Sourav Das/Sanjana - Hided Mr. X position for other players
    What changes have you made?
    
    * Sending XX instead of Mr. X position to all detectives
    
    Why do we need this change?
    
    * So that only Mr. X knows their position

--- src/handlers/serveLobbyStats.js
+++ src/handlers/serveLobbyStats.js
@@ -2,7 +2,13 @@ const serveLobbyStats = (req, res) => {
   const { game } = req.session;
   const { players, isGameStarted } = game.getStatus();
   const currentPlayer = players.find(player => player.username === req.session.username);
+  const mrX = players.find(player => player.role === 'Mr. X');
   const isHost = currentPlayer.isHost;
+
+  if (mrX && currentPlayer.role !== 'Mr. X') {
+    mrX.currentPosition = 'XX';
+  }
+
   res.json({ players, isGameStarted, isHost });
   return;
 };
