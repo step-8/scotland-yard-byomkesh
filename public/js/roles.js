0d18bfb90700728fa9d1bed30b7cdaef8cc8337e
sour-d <36912347+sour-d@users.noreply.github.com>
2022-08-12 18:48:25 +0530

    [#29] sourav das / sanjana - added roles names in frontend js file
    # What changes have you made?
    
    # *
    
    # Why do we need this change?
    
    # *
    
    # What can this change impact?
    
    # *

---
+++ public/js/roles.js
@@ -0,0 +1,8 @@
+const mrX = "Mr. X";
+const red = 'Detective Red';
+const green = 'Detective Green';
+const purple = 'Detective Purple';
+const blue = 'Detective Blue';
+const yellow = 'Detective Yellow';
+
+const roles = [mrX, red, green, purple, blue, yellow];
